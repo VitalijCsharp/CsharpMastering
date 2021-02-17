a7a0ccfcc456ca5407450ec7a7e4020e47f681ec
VitalijCsharp <kv1tosv@gmail.com>
2021-02-17 23:53:26 +0200

    MainTypesAndChapter1

---
+++ Chapter 1/TestHello/Program.cs
@@ -0,0 +1,21 @@
+﻿//using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestHello
+{
+    class Greating
+    {
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+            System.Console.ReadLine();//Ожидание, пока пользователь не нажмет ввод
+        }
+    }
+}
