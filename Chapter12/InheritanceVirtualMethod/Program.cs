85707e85672eca17298cc02c7b949764127b1daa
VitalijCsharp <kv1tosv@gmail.com>
2021-03-18 15:00:27 +0200

    Inheritance, overriding of virtual method, reassigning, extesive method for standart types.

---
+++ Chapter12/InheritanceVirtualMethod/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceVirtualMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Mammal[] arr = { new Horse(), new Whale(), new Human() ,new Delphine()};
+
+                foreach (var item in arr)
+                {
+                    Console.WriteLine(item.GetName());
+                }
+                Delphine del = new Delphine();
+                Console.WriteLine(del.GetName());
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+       
+            
+        
+    }
+}
---
+++ Chapter12/Vehicles/Vehicles/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Vehicles
+{
+    class Program
+    {
+        static void doWork()
+        {
+            Console.WriteLine("Journey by airplane :");
+            Airplane myAirplan = new Airplane();
+            myAirplan.StartEngine("br-br-br");
+            myAirplan.TakeOff();
+            myAirplan.Drive();
+            myAirplan.Land();
+            myAirplan.StopEngine("frfrfrfrfrfr");
+
+            Console.WriteLine("\nJourney by car :");
+            Car myCar = new Car();
+            myCar.StartEngine("jin-jin-jin");
+            myCar.Accelerate();
+            myCar.Drive();
+            myCar.Brake();
+            myCar.StopEngine("stop-stop-stop");
+
+            Console.WriteLine("\nTesting polymorphism");
+            Vehicle v = myCar;
+            v.Drive();
+            v = myAirplan;
+            v.Drive();
+        }
+
+        static void Main()
+        {
+            try
+            {
+                doWork();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: {0}", ex.Message);
+            }
+        }
+    }
+}
