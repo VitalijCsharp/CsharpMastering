1b012461b742d75db270aa7085fc99ce9b001d0e
VitalijCsharp <kv1tosv@gmail.com>
2021-03-25 21:08:39 +0200

    Indexators

---
+++ Chapter16/IndexatorExam/IntBits.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndexatorExam
+{
+    class IntBits
+    {
+        private int _bytes;
+        public IntBits(int item)
+        {
+            _bytes = item;
+        }
+        public int Bytes
+        {
+            get { return _bytes; }
+        }
+
+        public bool this[int index]
+        {
+            get => (_bytes & (1 << index)) != 0;
+            set
+            {
+                if (value)
+                {
+                    _bytes |= (1 << index);
+                }
+                else
+                {
+                    _bytes &= ~(1 << index);
+                }
+            }
+        }
+    }
+}
