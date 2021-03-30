5b1c2506b58ab0583e983e9b56c05670c644f0f7
VitalijCsharp <kv1tosv@gmail.com>
2021-03-30 23:14:10 +0300

    IEnumerable, IEnumerator, Iterators

---
+++ Chapter19/TestIterators/BasicCollection.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestIterators
+{
+    class BasicCollection<T> : IEnumerable<T>
+    {
+        List<T> data = null;
+
+        public BasicCollection()
+        {
+            data = new List<T>();
+        }
+        public void Fill(params T[] collection)
+        {
+            foreach (var item in collection)
+            {
+                data.Add(item);
+            }
+        }
+
+        public IEnumerable<T> Revers
+        {
+            get
+            {
+                for (int i = data.Count - 1; i >= 0; --i)
+                {
+                    yield return data[i];
+                }
+            }
+        }
+
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            foreach (var item in data)
+            {
+                yield return item;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
