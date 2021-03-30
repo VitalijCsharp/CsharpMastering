e34481bc83ea8f85e09ea07f26db974bbd0bcf7b
VitalijCsharp <kv1tosv@gmail.com>
2021-03-30 16:14:20 +0300

    Using Collection Generic classes in previous exercise

--- Chapter18/Cards/Cards/Pack.cs
+++ Chapter18/Cards/Cards/Pack.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Cards
 {
@@ -6,19 +7,47 @@ namespace Cards
     {
         public const int NumSuits = 4;
         public const int CardsPerSuit = 13;
-        private PlayingCard[,] cardPack;
+        //private PlayingCard[,] cardPack;
+        private Dictionary<Suit, List<PlayingCard>> cardPack;
         private Random randomCardSelector = new Random();
 
         public Pack()
         {
-            this.cardPack = new PlayingCard[NumSuits, CardsPerSuit];
+            //this.cardPack = new PlayingCard[NumSuits, CardsPerSuit];
+
+            this.cardPack = new Dictionary<Suit, List<PlayingCard>>(NumSuits);
+            //first ver
+            //for (Suit suit = Suit.Clubs; suit <= Suit.Spades; suit++)
+            //{
+            //    for (Value value = Value.Two; value <= Value.Ace; value++)
+            //    {                  
+            //        cardPack[suit].Add(new PlayingCard(suit, value));
+            //    }
+            //}
+
+            //Second ver
+            //List<PlayingCard> cardsInSuit = new List<PlayingCard>(CardsPerSuit);
+            //for (Suit suit = Suit.Clubs; suit <= Suit.Spades; suit++)
+            //{
+            //    for (Value value = Value.Two; value <= Value.Ace; value++)
+            //    {
+            //        cardsInSuit[(int)value] = new PlayingCard(suit, value);
+            //    }
+
+            //    cardPack[suit] = cardsInSuit;
+            //    cardsInSuit.Clear();
+            //}
+
+            //like in book
 
             for (Suit suit = Suit.Clubs; suit <= Suit.Spades; suit++)
             {
+                List<PlayingCard> cardsInSuit = new List<PlayingCard>(CardsPerSuit);
                 for (Value value = Value.Two; value <= Value.Ace; value++)
                 {
-                    this.cardPack[(int)suit, (int)value] = new PlayingCard(suit, value);
+                    cardsInSuit.Add(new PlayingCard(suit, value));
                 }
+                this.cardPack.Add(suit, cardsInSuit);
             }
         }
 
@@ -36,31 +65,35 @@ namespace Cards
             {
                 value = (Value)randomCardSelector.Next(CardsPerSuit);
             }
+            //original
+            //PlayingCard card = this.cardPack[suit, (int)value];            
+            //this.cardPack[(int)suit, (int)value] = null;
 
-            PlayingCard card = this.cardPack[(int)suit, (int)value];
-            this.cardPack[(int)suit, (int)value] = null;
+            //ver 1
+            //PlayingCard card = this.cardPack[suit][(int)value];
+            PlayingCard card = this.cardPack[suit].Find(c => c.CardValue == value);
+            cardPack[suit].Remove(card);
             return card;
         }
 
-        private bool IsSuitEmpty(Suit suit)
+        private bool IsSuitEmpty(Suit suit)// => !cardPack.ContainsKey(suit);
         {
-            bool result = true;
-            for (Value value = Value.Two; value <= Value.Ace; value++)
-            {
-                if (!IsCardAlreadyDealt(suit, value))
+                bool result = true;
+                for (Value value = Value.Two; value <= Value.Ace; value++)
                 {
-                    result = false;
-                    break;
+                    if (!IsCardAlreadyDealt(suit, value))
+                    {
+                        result = false;
+                        break;
+                    }                       
                 }
-            }
-
-            return result;
-
+                
+                return result;
         }
 
-        private bool IsCardAlreadyDealt(Suit suit, Value value)
+        private bool IsCardAlreadyDealt(Suit suit, Value value)// => !this.cardPack[suit].Contains(new PlayingCard(suit, value));
         {
-            return (this.cardPack[(int)suit, (int)value] == null);
+            return !this.cardPack[suit].Exists(c => c.CardValue == value);
         }
     }
 }
