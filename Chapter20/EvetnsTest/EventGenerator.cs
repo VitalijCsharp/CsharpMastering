319db6c1a3cf6405891e1c0749a756cca33f9d48
VitalijCsharp <kv1tosv@gmail.com>
2021-04-02 14:09:52 +0300

    Another test events

---
+++ Chapter20/EvetnsTest/EventGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvetnsTest
+{
+    class EventGenerator
+    {
+        public delegate void DelegateForEvent(string message);
+        public event DelegateForEvent MyEvent;
+
+        public void DoSomething()
+        {
+            foreach (var item in new string[] {"One", "Three", "Fore", "Five"})
+            {
+                Console.WriteLine(item);
+            }
+            //if(MyEvent != null)
+            //{
+            //    MyEvent("Event Invoked");
+            //}
+            MyEvent?.Invoke("Event Invoked");
+        }
+    }
+}
